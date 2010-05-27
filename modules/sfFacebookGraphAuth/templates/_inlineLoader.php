71a91802d32a5fd9ecfbb4980926b4ee046521bc
Kevin Dew <kev@kevin-dews-macbook.local>
2010-05-27 23:35:25 +0100

    now sync rathre than async

--- modules/sfFacebookGraphAuth/templates/_inlineLoader.php
+++ modules/sfFacebookGraphAuth/templates/_inlineLoader.php
@@ -4,33 +4,15 @@
  */
 ?>
 <div id="fb-root"></div>
+<?php include_facebook_javascripts($locale) ?>
 <?php javascript_tag(); ?>
-  window.fbAsyncInit = function() {
-    FB.init({appId: '<?php echo $apiKey ?>', status: <?php echo ($jsStatus ? 'true' : 'false') ?>,
-      cookie: <?php echo ($jsCookie ? 'true' : 'false') ?>, xfbml: <?php echo ($jsXfbml ? 'true' : 'false') ?>});
-    <?php if ($redirectOnNoSession) : ?>
-    FB.Event.subscribe('auth.sessionChange', function(response) {
-      if (response.session) {
-        window.location = '<?php echo url_for($signInUrl) ?>';
-      } else {
-        <?php if ($noSessionUrl) : ?>
-        window.location = '<?php echo url_for($noSessionUrl) ?>';
-        <?php else : ?>
-        document.location.reload();
-        <?php endif; ?>
-      }
-    });
-    <?php else : ?>
-    FB.Event.subscribe('auth.login', function(response) {
-      window.location = '<?php echo url_for($signInUrl) ?>';
-    });
-    <?php endif; ?>
-  };
-  (function() {
-    var e = document.createElement('script');
-    e.async = true;
-    e.src = document.location.protocol + 
-      '//connect.facebook.net/<?php echo $locale ?>/all.js';
-    document.getElementById('fb-root').appendChild(e);
-  }());
+    <?php include_partial('sfFacebookGraphAuth/inlineLoaderLogin', array(
+      'signInUrl' => $signInUrl,
+      'noSessionUrl' => $noSessionUrl,
+      'redirectOnNoSession' => $redirectOnNoSession,
+      'apiKey' => $apiKey,
+      'jsStatus' => $jsStatus,
+      'jsCookie' => $jsCookie,
+      'jsXfbml' => $jsXfbml
+    )) ?>
 <?php end_javascript_tag();
