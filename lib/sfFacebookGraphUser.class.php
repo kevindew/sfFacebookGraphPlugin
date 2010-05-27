c52097722b8ff997baa2314cf5debafe4cf78a18
Kevin Dew <kev@kevin-dews-macbook.local>
2010-05-27 23:34:31 +0100

    methods to check facebook authenticated

--- lib/sfFacebookGraphUser.class.php
+++ lib/sfFacebookGraphUser.class.php
@@ -12,7 +12,6 @@
 
 class sfFacebookGraphUser extends sfGuardSecurityUser
 {
-
   /**
    * Gets the facebook user id of the logged in user
    *
@@ -33,6 +32,56 @@ class sfFacebookGraphUser extends sfGuardSecurityUser
     }
 
     return null;
+  }
+
+  /**
+   * Method to determine whether a user was authenticated with facebook or not
+   *
+   * @return bool
+   */
+  public function isFacebookAuthenticated($default = null)
+  {
+    if ($default === null) {
+
+      $user = $this->getGuardUser();
+
+      // we'll guess the default off whether they have a facebook uid in their
+      // profile
+      $default = $user ? (bool) $user->getProfile()->getFacebookUid() : false;
+    }
+
+    return $this->getAttribute(
+      'facebook_authenticated',
+      $default,
+      'sfGuardSecurityUser'
+    );
+  }
+
+  /**
+   * Set whether a user was facebook authenticated or not
+   *
+   * @param   bool  $facebookAuthenticated
+   * @return  self
+   */
+  public function setFacebookAuthenticated($facebookAuthenticated)
+  {
+    // Using the sfGuardSecurityUser so that it can be removed when thats logged
+    // out
+    $this->setAttribute(
+      'facebook_authenticated',
+      $facebookAuthenticated,
+      'sfGuardSecurityUser'
+    );
+
+    return $this;
+  }
+
+  public function signIn(
+    $user, $remember = false, $con = null, $facebookAuthenticated = false
+  )
+  {
+    $this->setFacebookAuthenticated($facebookAuthenticated);
+    return parent::signIn($user, $remember, $con);
   }
 
   /**
